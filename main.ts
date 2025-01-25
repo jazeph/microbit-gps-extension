3c857ce41705ec4f8fe3195e03cf6cc1be03c5fc
jazeph <30824916+jazeph@users.noreply.github.com>
2025-01-25 17:26:43 +0100

    added semicolons

--- main.ts
+++ main.ts
@@ -1,59 +1,59 @@
 class GPS {
 
-    latitude: number
-    longitude: number
-    satellites: number
+    latitude: number;
+    longitude: number;
+    satellites: number;
 
     constructor() {
-        this.satellites = 0
+        this.satellites = 0;
     }
 
     init(tx: SerialPin, rx: SerialPin, baudRate: BaudRate) {
-        serial.redirect(tx, rx, baudRate)
+        serial.redirect(tx, rx, baudRate);
         // serial.setRxBufferSize(200)
     }
 
     update(latitudeStr: string, latitudeDir: string, longitudeStr: string, longitudeDir: string, satellitesStr: string) {
-        this.latitude = (latitudeDir == "N" ? 1 : -1) * parseFloat(removeLeadingZeros(latitudeStr))
-        this.longitude = (longitudeDir == "E" ? 1 : -1) * parseFloat(removeLeadingZeros(longitudeStr))
-        this.satellites = parseFloat(removeLeadingZeros(satellitesStr))
+        this.latitude = (latitudeDir == "N" ? 1 : -1) * parseFloat(removeLeadingZeros(latitudeStr));
+        this.longitude = (longitudeDir == "E" ? 1 : -1) * parseFloat(removeLeadingZeros(longitudeStr));
+        this.satellites = parseFloat(removeLeadingZeros(satellitesStr));
     }
 
     toString() {
-        return "lat=" + this.latitude + " lon=" + this.longitude + " sat=" + this.satellites
+        return "lat=" + this.latitude + " lon=" + this.longitude + " sat=" + this.satellites;
     }
 }
 
 function removeLeadingZeros(str: string) {
-    let removed_leading_zeros = str
+    let removed_leading_zeros = str;
     while (removed_leading_zeros.substr(0, 1) == "0") {
-        removed_leading_zeros = removed_leading_zeros.substr(1, removed_leading_zeros.length - 1)
+        removed_leading_zeros = removed_leading_zeros.substr(1, removed_leading_zeros.length - 1);
     }
-    return removed_leading_zeros
+    return removed_leading_zeros;
 }
 
-let gps = new GPS()
+let gps = new GPS();
 
 basic.forever(function () {
 
-    let nmeaField = serial.readUntil(serial.delimiters(Delimiters.Comma))
+    let nmeaField = serial.readUntil(serial.delimiters(Delimiters.Comma));
 
     if (nmeaField.includes("$GPGGA")) {
 
-        serial.readUntil(serial.delimiters(Delimiters.Comma)) // ignore
+        serial.readUntil(serial.delimiters(Delimiters.Comma)); // ignore
 
-        let latitude = removeLeadingZeros(serial.readUntil(serial.delimiters(Delimiters.Comma)))
-        let latitudeDir = serial.readUntil(serial.delimiters(Delimiters.Comma))
-        let longitude = serial.readUntil(serial.delimiters(Delimiters.Comma))
-        let longitudeDir = serial.readUntil(serial.delimiters(Delimiters.Comma))
+        let latitude = serial.readUntil(serial.delimiters(Delimiters.Comma));
+        let latitudeDir = serial.readUntil(serial.delimiters(Delimiters.Comma));
+        let longitude = serial.readUntil(serial.delimiters(Delimiters.Comma));
+        let longitudeDir = serial.readUntil(serial.delimiters(Delimiters.Comma));
 
-        serial.readUntil(serial.delimiters(Delimiters.Comma)) // ignore
+        serial.readUntil(serial.delimiters(Delimiters.Comma)); // ignore
 
-        let satellites = serial.readUntil(serial.delimiters(Delimiters.Comma))
+        let satellites = serial.readUntil(serial.delimiters(Delimiters.Comma));
 
-        gps.update(latitude, latitudeDir, longitude, longitudeDir, satellites)
+        gps.update(latitude, latitudeDir, longitude, longitudeDir, satellites);
 
-        basic.showString(gps.toString())
+        basic.showString(gps.toString());
     }
 })
 
@@ -68,7 +68,7 @@ namespace GPS {
     //% rx.defl=SerialPin.P1
     //% baudRate.defl=BaudRate.BaudRate9600
     export function init(tx: SerialPin, rx: SerialPin, baudRate: BaudRate) {
-        gps.init(tx, rx, baudRate)
+        gps.init(tx, rx, baudRate);
     }
 
     //% block
